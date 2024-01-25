d64410c33ddd480c6807731462a6558f2e393a81
Mohamed <moh.hazem321@gmail.com>
2024-01-25 13:18:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# Signal Viewer Application
+
+The Signal Viewer Application is a PyQt5-based tool for visualizing and analyzing signals using Pyqtgraph.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Team Members](#Team-Members)
+- [Features](#features)
+- [Video Demonstration](#Video-Demonstration)
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+
+
+## Introduction
+
+The Signal Viewer Application allows users to visualize and analyze signals in real-time, providing a feature-rich interface for signal manipulation and data analysis.
+
+## Team Members
+The following team members have contributed to this project:
+- Mohamed Hazem Yehya
+- Assem Hussein
+- Kyrolos Emad
+- Arsany Maged
+
+## Features
+
+- **Signal Loading and Display:** Load signals from CSV files and display them dynamically in a user-friendly interface.
+
+- **Play/Pause Signals:** Toggle between playing and pausing the signals for dynamic visualization.
+
+- **Signal Color Customization:** Customize the color of individual signals for easy identification and analysis.
+
+- **Signal Legend:** Automatically generate and display legends for each signal, providing additional context.
+
+- **Multiple Graph Widgets:** Utilize multiple graph widgets to compare and analyze signals simultaneously.
+
+- **Signal Manipulation:** Easily manipulate signals by zooming in, zooming out, and moving the signals between the two displays easily.
+
+- **Signal Snapshots PDF Report Generation:** Capture snapshots of the displayed signals and generate a PDF report containing statistical analysis and visualizations.
+
+- **Data Analysis:** Perform data analysis on loaded signals, including mean, median, standard deviation, maximum, and minimum values.
+
+## Video Demonstration
+
+![Watch the Demo Video](Demo/App_Demo.mp4)
+
+## Requirements
+
+List the dependencies and requirements needed to run the application.
+
+- Python 3.6+
+- PyQt5
+- Matplotlib
+- Numpy
+- Sympy
+- Pyqtgraph
+- Pandas
+- Reportlab
+
+## Installation
+
+Install the required dependencies using pip
+
+```bash
+pip install PyQt5 matplotlib numpy sympy pyqtgraph pandas reportlab
+```
+
+## Usage
+Run the application:
+```bash
+python main_window.py
+```
+
+## Contribution
+Contributions to Signal Viewer are welcome! If you encounter any issues or have suggestions for improvements, please create a new issue or submit a pull request.
+
+When contributing, please ensure that you follow the existing coding style and include clear commit messages to maintain a well-documented project history.
---
+++ main_window.py
@@ -0,0 +1,159 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QWidget, QGraphicsView, QDialog, QGridLayout, QShortcut
+from PyQt5.QtCore import Qt
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QKeySequence
+import numpy as np
+from class_SignalViewer import class_signal_viewer
+import sys
+
+class main_window(QMainWindow):
+    def __init__(self):
+        super(main_window, self).__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.linking_enabled = False
+        # Used to handle unlinking the x axis during signal playback
+        self.link_x_range_of_views = False
+
+        self.activeWidget = None
+        
+        # Creating and setting menu bar
+        menuBar = self.menuBar()
+        self.setMenuBar(menuBar)
+        
+        # Create Signals menu
+        signalMenu = QtWidgets.QMenu("&File", self)
+        menuBar.addMenu(signalMenu)
+        
+        # # Add open action to File Menu
+        # self.openAction = QtWidgets.QAction("&Open", self)
+        # signalMenu.addAction(self.openAction)
+        
+        # Adding rename action to File Menu
+        self.renameAction = QtWidgets.QAction("&Rename Signal", self)
+        signalMenu.addAction(self.renameAction)
+
+        # Adding Export action to File Menu
+        self.exportAction = QtWidgets.QAction("&Export PDF", self)
+        signalMenu.addAction(self.exportAction)
+
+        self.setMinimumSize(1200, 600)
+
+        self.container = QWidget()
+        self.layout = QtWidgets.QHBoxLayout()
+
+        self.signal_view_1 = class_signal_viewer(Qt.Key_O, Qt.Key_Z, Qt.Key_X, Qt.Key_C, Qt.Key_P, Qt.Key_R, Qt.Key_A, Qt.Key_D, Qt.Key_S)
+        self.signal_view_2 = class_signal_viewer(QKeySequence('shift+O'), QKeySequence('shift+Z'), QKeySequence('shift+X'), QKeySequence('shift+C'), QKeySequence('shift+P'), QKeySequence('shift+R'), QKeySequence('shift+A'), QKeySequence('shift+D'), QKeySequence('shift+S'))
+        self.layout.addWidget(self.signal_view_1)
+        self.layout.addWidget(self.signal_view_2)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.addLayout(self.layout)
+        self.setCentralWidget(self.container)
+        self.container.setLayout(self.verticalLayout)
+        self.signal_view_1.setFocus()
+
+        # Setting up signal tranfer by linking views
+        self.signal_view_1.btn_transfer.clicked.connect(lambda: self.signal_view_1.move_signal(self.signal_view_2))
+        self.signal_view_2.btn_transfer.clicked.connect(lambda: self.signal_view_2.move_signal(self.signal_view_1))
+    
+        
+        self.exportAction.triggered.connect(lambda: self.signal_view_1.save_screenshots_as_pdf(self.signal_view_2.screenshots, self.signal_view_2.loaded_signals))
+        self.renameAction.triggered.connect(self.rename_active_widget)
+        
+        
+        self.signal_view_1.signal_emitter.sigPlotClicked.connect(self.signal_view_2.deselect_signal)
+        self.signal_view_1.signal_emitter.sigPlotClicked.connect(lambda: self.mark_active_widget(self.signal_view_1))
+        self.signal_view_2.signal_emitter.sigPlotClicked.connect(self.signal_view_1.deselect_signal)
+        self.signal_view_2.signal_emitter.sigPlotClicked.connect(lambda: self.mark_active_widget(self.signal_view_2))
+        
+        self.signal_view_1.btn_link.clicked.connect(self.toggle_linking)
+        self.signal_view_2.btn_link.clicked.connect(self.toggle_linking)
+        
+    
+    def mark_active_widget(self, widget):
+        self.activeWidget = widget
+    
+    def rename_active_widget(self):
+        self.activeWidget.rename_selected_signal()
+
+
+
+
+    # Function the toggles Linking the views
+    def toggle_linking(self):
+        # Toggle linking flag
+        self.linking_enabled = not self.linking_enabled
+        
+        if self.linking_enabled:
+
+            # set is_linked flag in both views
+            self.signal_view_1.is_linked = self.signal_view_2.is_linked = True
+
+            self.signal_view_1.view_widget.setXLink(self.signal_view_2.view_widget)
+            self.signal_view_1.view_widget.setYLink(self.signal_view_2.view_widget)
+            
+            self.signal_view_2.current_index = self.signal_view_1.current_index
+        
+
+         # Change UI for linked mode
+            self.signal_view_2.btn_add_signal.setEnabled(False)
+            self.signal_view_2.btn_link.setEnabled(False)
+            self.signal_view_2.btn_restart.setEnabled(False)
+            self.signal_view_2.btn_start_pause.setEnabled(False)
+            self.signal_view_2.btn_zoom_out.setEnabled(False)
+            self.signal_view_2.btn_zoom_in.setEnabled(False)
+
+        # Control view 2 with view 1's buttons
+            self.signal_view_1.btn_start_pause.clicked.connect(self.signal_view_2.toggle_animation)
+
+            self.signal_view_1.btn_clear.clicked.connect(self.signal_view_2.clear_signals)
+            self.signal_view_1.btn_restart.clicked.connect(self.signal_view_2.reset_animation)
+            self.signal_view_1.dial_speed.valueChanged.connect(lambda value: self.signal_view_2.dial_speed.setValue(value))
+            
+            # Toggle animation if either of the two plots was already running
+            
+            if self.signal_view_1.animation_running or self.signal_view_2.animation_running:
+                self.signal_view_1.start_animation()
+                self.signal_view_2.start_animation()
+            
+            # Setup both views
+            self.signal_view_1.view_widget.setXRange(0, 1000)
+            self.signal_view_1.view_widget.setYRange(-1.3, 1.3)
+
+  
+
+        else:
+            # set is_linked flag in both views
+            self.signal_view_1.is_linked = self.signal_view_2.is_linked = False
+            
+            self.signal_view_1.view_widget.setXLink(None)
+            self.signal_view_1.view_widget.setYLink(None)
+
+            # Return controls to normal
+            self.signal_view_1.btn_restart.clicked.disconnect(self.signal_view_2.reset_animation)
+            self.signal_view_1.btn_clear.clicked.disconnect(self.signal_view_2.clear_signals)
+            
+            self.signal_view_1.btn_start_pause.clicked.disconnect(self.signal_view_2.toggle_animation)
+            
+           
+            # Return UI to normal
+            self.signal_view_2.btn_restart.setEnabled(True)
+            self.signal_view_2.btn_add_signal.setEnabled(True)
+            self.signal_view_2.btn_link.setEnabled(True)
+            self.signal_view_2.btn_start_pause.setEnabled(True)
+            self.signal_view_2.btn_zoom_out.setEnabled(True)
+            self.signal_view_2.btn_zoom_in.setEnabled(True)
+            self.signal_view_1.dial_speed.valueChanged.disconnect()
+            self.signal_view_1.dial_speed.valueChanged.connect(self.signal_view_1.update_speed)
+                       
+
+    
+        QApplication.processEvents()
+
+
+
+app = QApplication(sys.argv)
+win = main_window()
+win.show()
+app.exec()
